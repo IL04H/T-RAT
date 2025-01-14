2b278a41e5572af039dae72ad44cf9a9da2b1c01
IL04H <tiktokphotocomment7777@gmail.com>
2025-01-14 18:18:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '972949824'
+const token = '7517283178:AAEylxjPuFUZGLZoM6V23J8LxgjDnCiodFU'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
